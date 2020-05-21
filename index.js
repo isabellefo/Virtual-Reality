d66ae51ed669d4233ae74f72d7d1cb4bc29c8857
Isabelle Oliveira <50581090+isabellefo@users.noreply.github.com>
2020-05-21 16:14:35 -0300

    Metodo click

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("hello")
+cont = 1;
+document.querySelector("#btn1").addEventListener("click", function () {
+
+  var el = document.querySelector("#peça1");
+  var btn1 = document.querySelector("#btn1");
+  var text = document.querySelector("#text");
+
+  el.object3D.visible = true;
+  btn1.object3D.visible = false;
+  text.object3D.visible = false;
+ 
+});
