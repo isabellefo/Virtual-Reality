50d8d9fb910508ae96f2781af109cf0e82b3ee24
Gabriel Borges <gabrielfr.borges@gmail.com>
2020-05-29 09:12:56 -0300

    func move para mover uma peça

--- index.js
+++ index.js
@@ -8,5 +8,19 @@ document.querySelector("#btn1").addEventListener("click", function () {
   el.object3D.visible = true;
   btn1.object3D.visible = false;
   text.object3D.visible = false;
+  //moveup(el);
+  
  
 });
+
+function move(el,movement){
+  console.log("what?")
+  console.log(el);
+  let position = el.getAttribute("position");
+  for(let[ cord , value] of Object.entries(position)){
+    position[cord] = value + movement[cord];
+  }
+  
+  el.setAttribute("position", position);
+  
+}
