bad3b1db9989d3e41f270ced15d5c7b2248d4462
Gabriel Borges <gabrielfr.borges@gmail.com>
2020-06-19 08:10:43 -0300

    wrapper nos botões

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 cont = 1;
 const int horizontal = 0.5;
 const int vertical = 0.1
-document.querySelector("#btn1").addEventListener("click", function () {
-
+document.querySelector("#startBtn").addEventListener("click", function () {
+  
   var el = document.querySelector("#peça1");
-  var btn1 = document.querySelector("#btn1");
-  var text = document.querySelector("#text");
+  var btn1 = document.querySelector("#startBtn");
+  var text = document.querySelector("#startText");
+  var panel = document.getElementById("panel");
 
   el.object3D.visible = true;
   btn1.object3D.visible = false;
   text.object3D.visible = false;
+  panel.visible = true;
  
 });
 
